5c1244d2df668e1e024ef427847238304978ae99
Acroyna <acroyna@gmail.com>
2024-11-12 22:10:32 +0530

    testing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-blue-800 text-white p-4 text-center">
-      <p className="text-white">&copy; {new Date().getFullYear()} Acroyna Software Solutions. All rights reserved.</p>
+      <p className="text-white">&copy; {new Date().getFullYear()} Acroyna Software Solutionsss. All rights reserved.</p>
     </footer>
   );
 }
